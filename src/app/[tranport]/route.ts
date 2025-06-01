61e5189bd60dee523d327ce3d7518489cdd05b0e
gynflo <ginet.florian.dev@gmail.com>
2025-06-01 14:41:17 +0200

    first commit

---
+++ src/app/[tranport]/route.ts
@@ -0,0 +1,44 @@
+import { createMcpHandler } from "@vercel/mcp-adapter";
+import { z } from "zod";
+
+const handler = createMcpHandler(
+  (server) => {
+    server.tool(
+      "courseRecommended",
+      "Give a course recommandation based on experience level",
+      {
+        experienceLevel: z.enum(["beginner", "intermediate"]),
+      },
+      ({ experienceLevel }) => ({
+        content: [
+          {
+            type: "text",
+            text: `I recommend you take the ${
+              experienceLevel === "beginner"
+                ? "Professionnal Javascript"
+                : "Professional React & Nextjs"
+            } course`,
+          },
+        ],
+      })
+    );
+  },
+  {
+    capabilities: {
+      tools: {
+        courseRecommended: {
+          description: "Give a course recommandation based on experience level",
+        },
+      },
+    },
+  },
+  {
+    redisUrl: process.env.REDIS_URL,
+    sseEndpoint: "/sse",
+    streamableHttpEndpoint: "/mcp",
+    verboseLogs: true,
+    maxDuration: 60,
+  }
+);
+
+export { handler as GET, handler as POST, handler as DELETE };
