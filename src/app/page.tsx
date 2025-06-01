8ed6e7e57e11124ac8568a48408a5bae4bf00252
gynflo <ginet.florian.dev@gmail.com>
2025-06-01 15:36:51 +0200

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-3xl font-bold underline">Welcome to the Home Page</h1>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to the Home Page Gynflo Grad
+      </h1>
       <p>
-        This is the home page of the website, it is a simple page that displays
-        a welcome message and a link to the about page.
+        This is the home page of the website GynfloGrad, it is a simple page
+        that displays a welcome message and a link to the about page.
       </p>
     </div>
   );
